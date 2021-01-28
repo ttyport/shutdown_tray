2174bd378ceb6c85c748ac661091c591474a1be2
yayguy4618 <yayguy.4618@yandex.com>
2021-01-28 13:25:41 +0900

    New icon and bugs fixed

--- tray.py
+++ tray.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from sys import argv, exit
+import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QStyleFactory
 
@@ -10,20 +10,32 @@ from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QStyl
 class Tray(QSystemTrayIcon):
     def __init__(self):
         super().__init__()
-        self.setIcon(QIcon("/usr/share/shutdown_tray/icon.png"))
+
+        if os.path.exists("/usr/share/shutdown_tray/icon.png"):
+            self.setIcon(QIcon("/usr/share/shutdown_tray/icon.png"))
+        elif os.path.exists("icon.png"):
+            self.setIcon(QIcon("icon.png"))
+
         menu = QMenu()
+
         shutdown = QAction("Shutdown", self)
         shutdown.triggered.connect(shutdown_func)
+
+        exit = QAction("Exit", self)
+        exit.triggered.connect(sys.exit)
+
         menu.addAction(shutdown)
+        menu.addAction(exit)
+
         self.setContextMenu(menu)
 
 
 def shutdown_func():
-    os.system("shutdown now")
+    os.system("sudo shutdown now")
 
 
 if __name__ == '__main__':
-    app = QApplication(argv)
+    app = QApplication(sys.argv)
     tray = Tray()
     tray.show()
-    exit(app.exec())
+    sys.exit(app.exec())
